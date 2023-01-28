7ed9769996a9e004cbda3dce6eb42e09d1249bc8
Ali Hassan <alihassan4198@gmail.com>
2023-01-29 00:23:38 +0500

    Update app.py

--- selenium_bot/app.py
+++ selenium_bot/app.py
@@ -19,8 +19,8 @@ def lambda_handler(event, context):
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--headless')
         chrome_options.add_argument('--disable-dev-shm-usage')
-        # chrome_options.add_argument('--disable-gpu')
-        # chrome_options.add_argument('--disable-gpu-sandbox')
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--disable-gpu-sandbox')
         chrome_options.add_argument("--single-process")
 
         driver = webdriver.Chrome(
